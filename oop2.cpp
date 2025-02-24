c66cc4a29d71846ff901dcdc7a8418eb79d2257e
Saruma01 <annarudakova2005@gmail.com>
2025-02-24 14:42:01 +0500

    main
    1 commit

--- oop2.cpp
+++ oop2.cpp
@@ -5,28 +5,6 @@ public:
     int size;
     string color, gender;
 
-    Clother() {
-        cout << "Clother()\n";
-        size = 0;
-        color = "";
-        gender = "";
-    }
-    Clother(int size, string color, string gender) {
-        cout << "Clother(int size, string color, string gender)\n";
-        this->size = size;
-        this->color = color;
-        this->gender = gender;
-    }
-    Clother(const Clother &p) {
-        cout << "Clother(const Clother &p)\n";
-        size = p.size;
-        color = p.color;
-        gender = p.gender;
-    }
-    ~Clother() {
-        cout << "~Clother()\n";
-        cout  << size << " " << color << " " << gender << endl;
-    }
     void print_clothers() const {
 
         cout << "size: " << size << ", color: " << color << ", gender: " << gender << endl;
@@ -35,8 +13,17 @@ public:
 };
 
 int main()
-{   
-    Clother a1;
-    Clother a2(42,"red","female");
-    Clother a3(a2);
+{
+    Clother a;
+    a.size = 42;
+    a.color = "red";
+    a.gender = "female";
+    a.print_clothers();
+
+    Clother *b = new Clother();
+    b->size = 34;
+    b->color = "blue";
+    b->gender = "male";
+    b->print_clothers();
 }
+
